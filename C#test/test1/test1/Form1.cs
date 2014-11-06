d3de6d67d37ebbfaedfc50d21669f4caf53077fb
code <liangqian0723@gmail.com>
2014-11-06 21:26:06 +0800

    **提交自己写的第一个C#程序，虽然写的很一般，但是知道是什么回事了

---
+++ C#test/test1/test1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("C:\\Users\\Channe\\Desktop\\下载.png","about");
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("202.206.223.102","about");
+        }
+    }
+}
